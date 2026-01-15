4e561dd84a8b11b9c7907ecd18013df3a2d6b0bc
AlfinAkash <alfinakash2001@gmail.com>
2026-01-15 20:12:22 +0530

    fix: updated the mongodb config

--- src/main/java/com/techlambdas/invoice_generator/config/MongoConfig.java
+++ src/main/java/com/techlambdas/invoice_generator/config/MongoConfig.java
@@ -1,23 +1,23 @@
- package com.techlambdas.invoice_generator.config;
+//  package com.techlambdas.invoice_generator.config;
 
- import com.mongodb.client.MongoClient;
- import com.mongodb.client.MongoClients;
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.data.mongodb.core.MongoTemplate;
+//  import com.mongodb.client.MongoClient;
+//  import com.mongodb.client.MongoClients;
+//  import org.springframework.context.annotation.Bean;
+//  import org.springframework.context.annotation.Configuration;
+//  import org.springframework.data.mongodb.core.MongoTemplate;
 
- @Configuration
- public class MongoConfig {
+//  @Configuration
+//  public class MongoConfig {
 
-     @Bean
-     public MongoClient mongoClient() {
-         return MongoClients.create(
-                 "add your mongodb atlas connection string here"
-         );
-     }
+//      @Bean
+//      public MongoClient mongoClient() {
+//          return MongoClients.create(
+//                  "add your mongodb atlas connection string here"
+//          );
+//      }
 
-     @Bean
-     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-         return new MongoTemplate(mongoClient, "invoice_db");
-     }
- }
+//      @Bean
+//      public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+//          return new MongoTemplate(mongoClient, "invoice_db");
+//      }
+//  }
